8349062c7655269177aefa559b34f86170497ef0
rhammock1 <robert.hammock@gotab.io>
2026-02-28 21:11:56 -0500

    feat: basic auth functions

---
+++ lib/actions/auth.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import {redirect} from 'next/navigation';
+import {createClient} from '@/lib/supabase/server';
+import {actionClient} from '@/lib/actions/action-client';
+
+export async function login(values: {email: string, password: string}) {
+  return actionClient(async () => {
+    const sb = await createClient();
+    const {error} = await sb.auth.signInWithPassword(values);
+    if(error) {
+      throw error;
+    }
+    redirect('/dashboard?confetti=true');
+  });
+}
+
+export async function signup(values: {email: string, password: string}) {
+  return actionClient(async () => {
+    const sb = await createClient();
+    const {error} = await sb.auth.signUp(values);
+    if(error) {
+      throw error;
+    }
+    redirect('/dashboard?confetti=true');
+  });
+}
+
+export async function logout() {
+  return actionClient(async () => {
+    const sb = await createClient();
+    await sb.auth.signOut();
+    redirect('/login');
+  });
+}
