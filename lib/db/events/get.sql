000d961154ebf9a1907d6336a29b926c203e69b9
rhammock1 <robert.hammock@gotab.io>
2026-03-01 11:41:27 -0500

    feat: get events and search events queries

---
+++ lib/db/events/get.sql
@@ -0,0 +1,19 @@
+SELECT e.event_id,
+  e.event_name,
+  e.event_description,
+  e.event_start_time,
+  e.event_end_time,
+  e.event_sport_type,
+  e.event_created,
+  e.create_user_id,
+  COALESCE(
+    jsonb_agg(jsonb_build_object(
+      'venue_id', v.venue_id,
+      'venue_name', v.venue_name
+    )) FILTER WHERE v.venue_id IS NOT NULL,
+    '[]'::jsonb
+  ) AS event_venues
+FROM events e
+LEFT JOIN event_venues ev ON ev.event_id = e.event_id AND ev.archived IS NULL
+LEFT JOIN venues v ON v.venue_id = ev.venue_id AND v.archived IS NULL
+WHERE e.archived IS NULL;
---
+++ lib/db/events/get_by_id_with_venues.sql
@@ -0,0 +1,24 @@
+SELECT e.event_id,
+  e.event_name,
+  e.event_description,
+  e.event_start_time,
+  e.event_end_time,
+  e.event_sport_type,
+  e.event_created,
+  e.create_user_id,
+  COALESCE(
+    jsonb_agg(jsonb_build_object(
+      'venue_id', v.venue_id,
+      'venue_name', v.venue_name,
+      'venue_description', v.venue_description,
+      'venue_address', formatAddress(v.venue_street_1, v.venue_street_2, v.venue_city, v.venue_state, v.venue_zip),
+      'venue_created', v.venue_created,
+      'venue_create_user_id', v.create_user_id
+    )) FILTER WHERE v.venue_id IS NOT NULL,
+    '[]'::jsonb
+  ) AS event_venues
+FROM events e
+LEFT JOIN event_venues ev ON ev.event_id = e.event_id AND ev.archived IS NULL
+LEFT JOIN venues v ON v.venue_id = ev.venue_id AND v.archived IS NULL
+WHERE e.archived IS NULL
+  AND e.event_id = ${event_id};
