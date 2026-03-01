92a9b3d22008f8b7cf0185260a544d62a1179cb1
rhammock1 <robert.hammock@gotab.io>
2026-02-28 20:33:07 -0500

    fix: ensure db.upgrade runs in nodejs runtime

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,11 @@
-import * as db from "@/lib/db/index";
-
 export async function register() {
-  try {
-    await db.upgrade();
-  } catch(err) {
-    console.error('[DB] Error upgrading the database. Shutting down:', err);
-    process.exit(1);
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    try {
+      const db = await import('@/lib/db/index');
+      await db.upgrade();
+    } catch(err) {
+      console.error('[DB] Error upgrading the database. Shutting down:', err);
+      process.exit(1);
+    }
   }
 }
