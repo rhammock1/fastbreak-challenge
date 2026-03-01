66231837231890be2eb04a774323016d0f6b8c9a
rhammock1 <robert.hammock@gotab.io>
2026-03-01 15:41:47 -0500

    fix: refactor query based on usage

--- lib/db/events/get_by_uuid_with_venues.sql
+++ lib/db/events/get_by_uuid_with_venues.sql
@@ -13,14 +13,15 @@ SELECT e.event_id,
       'venue_uuid', v.venue_uuid,
       'venue_name', v.venue_name,
       'venue_description', v.venue_description,
-      'venue_address', format_address(v.venue_street_1, v.venue_street_2, v.venue_city, v.venue_state, v.venue_zip),
+      'venue_address', format_address(v.venue_street_1, v.venue_street_2, v.venue_city, v.venue_state, v.venue_zip, v.venue_country),
       'venue_created', v.venue_created,
       'venue_create_user_id', v.create_user_id
-    )) FILTER WHERE v.venue_id IS NOT NULL,
+    )) FILTER (WHERE v.venue_id IS NOT NULL),
     '[]'::jsonb
   ) AS event_venues
 FROM events e
 LEFT JOIN event_venues ev ON ev.event_id = e.event_id AND ev.archived IS NULL
 LEFT JOIN venues v ON v.venue_id = ev.venue_id AND v.archived IS NULL
 WHERE e.archived IS NULL
-  AND e.event_id = ${event_id};
+  AND e.event_uuid = ${event_uuid}
+GROUP BY e.event_id;
