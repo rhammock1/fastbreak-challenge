fe7c3b2ec5f697233021d584592c28e334690a96
rhammock1 <robert.hammock@gotab.io>
2026-03-02 08:56:31 -0500

    feat: add a background

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import {LightWavesBackground} from "@/components/ui/light-waves";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-white`}
       >
-        {children}
+        <LightWavesBackground speed={0.25}>
+          {children}
+        </LightWavesBackground>
         <Toaster />
       </body>
     </html>
