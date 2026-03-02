6417cc4bb005b28167c6c0aa958b4226141f8996
rhammock1 <robert.hammock@gotab.io>
2026-03-02 09:08:37 -0500

    fix: syntax prevented the actual updates from occuring

--- lib/db/events/patch.sql
+++ lib/db/events/patch.sql
@@ -1,8 +1,8 @@
 UPDATE events
-SET event_name = COALESCE(event_name, ${event_name}),
-  event_description = COALESCE(event_description, ${event_description}),
-  event_sport_type = COALESCE(event_sport_type, ${event_sport_type}),
-  event_start_time = COALESCE(event_start_time, ${event_start_time}),
-  event_end_time = COALESCE(event_end_time, ${event_end_time})
+SET event_name = COALESCE(${event_name}, event_name),
+  event_description = COALESCE(${event_description}, event_description),
+  event_sport_type = COALESCE(${event_sport_type}, event_sport_type),
+  event_start_time = COALESCE(${event_start_time}, event_start_time),
+  event_end_time = COALESCE(${event_end_time}, event_end_time)
 WHERE event_uuid = ${event_uuid}
 RETURNING event_id;
